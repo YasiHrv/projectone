b8388f5b7aba3116f10801d8561a71d66e1a0d54
YasiHrv <heraviyasaman@gmail.com>
2024-04-08 16:03:45 +0430

    Add to remote GitHub

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+
+const App= () => {
+    return <div>Hello yasaman</div>;
+};
+
+
+ReactDOM.render(
+<App/>,
+document.querySelector('#root')
+);
